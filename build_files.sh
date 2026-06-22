f568ddb861992829946a5105e9446f7b72aeb10f
Sunday <sunnexajayi@gmail.com>
2026-06-22 13:46:06 +0200

    chore: update Vercel build configuration and include migrations and data seeding in build script

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
+python3 -m pip install -r requirements.txt --break-system-packages
+python3 manage.py collectstatic --noinput
+python3 manage.py migrate --noinput
+python3 manage.py seed_initial_data
